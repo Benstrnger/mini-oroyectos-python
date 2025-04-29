724b661c45359da3d8ae6b13fa18deb5a3e87f2c
Benjamin Vergara <benjavergara90@gmail.com>
2025-04-28 23:21:33 -0400

    Se sigue mejorando adivina_el_numero, nuevas funcionalidades

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,6 +1,7 @@
 # Mini proyecto ADIVINA EL NUMERO
 # Se genera un numero aleatorio entre 1 a 100 y el usuario debe adivinar el numero
 import random
+import time
 
 def pedir_numero():
     while True:
@@ -15,54 +16,90 @@ def pedir_numero():
             print("ERROR: Ingresó un valor no válido")
 
 def main():
+    puntaje_total = 0
+
     print("-" * 40)
     print("🎯 ¡Bienvenido a Adivina el Número! 🎯")
     print("Estoy pensando en un número entre 1 y 100.")
     print("-" * 40)
-
+    
+    # Bucle de la run
     while True:
         numero_secreto = random.randint(1,100)
         intentos = 0
         minimo = 1
         maximo = 100
-        modo_dificil = False
+        max_intentos = 0
+        
+        nivel = input("Elige dificultad: (f)ácil, (n)ormal, (d)ifícil, (i)mposible: ").lower()
 
-        modo = input("¿Desea jugar en modo difícil (s/n)? ").lower()
-        if modo == "s":
-            modo_dificil = True
+        if nivel == "f":
+            max_intentos = 20
+        elif nivel == "n":
+            max_intentos = 15
+        elif nivel == "d":
+            max_intentos = 10
+        elif nivel == "i":
+            max_intentos = 5
         else:
-            modo_dificil = False
+            max_intentos = 15
+
 
         print("\n" + "-" * 40)
         print("¡Nueva partida! ¡Buena suerte!")
         print("-" * 40)
 
-        # Bucle de adivinanza
+        print("Pensando un numero", end="", flush=True)
+        for _ in range(3):
+            time.sleep(0.5)
+            print(".", end="", flush=True)
+        print()
+
+        # Bucle de la adivinanza
         while True:
             n = pedir_numero()
             intentos += 1
-            if n > numero_secreto:
-                print(f"Menor que {n}")
-                maximo = n - 1
-                print(f"Pista: el número está entre {minimo} y {maximo}.")
+            if n == numero_secreto:
+                if 1 <= intentos <= 3:
+                    print(f"¡Increíble! ¡Adivinaste el número secreto: {numero_secreto}, en apenas {intentos} intentos!")
+                    puntaje_total += 5
+                elif 4 <= intentos <= 7:
+                    print(f"¡Increíble! ¡Adivinaste el número secreto: {numero_secreto}, en apenas {intentos} intentos!")
+                    puntaje_total += 3
+                elif intentos >= 8:
+                    print(f"¡CORRECTO! Adivinaste el número secreto: {numero_secreto}, en {intentos} intentos.")
+                    puntaje_total += 1
+                break
             elif n < numero_secreto:
                 print(f"Mayor que {n}")
                 minimo = n + 1
                 print(f"Pista: el número está entre {minimo} y {maximo}.")
             else:
-                if intentos <= 5:
-                    print(f"¡Increíble! ¡Adivinaste el número secreto: {numero_secreto}, en apenas {intentos} intentos!")
-                else:
-                    print(f"¡CORRECTO! Adivinaste el número secreto: {numero_secreto}, en {intentos} intentos.")
-                break
-            if modo_dificil == True and intentos >= 10:
-                print("No lograste Adivinar antes de los 10 intentos. PERDISTE😈😈😈")
+                print(f"Menor que {n}")
+                maximo = n - 1
+                print(f"Pista: el número está entre {minimo} y {maximo}.")
+
+            
+            diferencia = abs(numero_secreto - n)
+            if diferencia <= 5:
+                print("🔥 Muy caliente")
+            elif diferencia <= 15:
+                print("🔥 Caliente")
+            elif diferencia <= 30:
+                print("❄️ Frío")
+            else:
+                print("🥶 Muy frío")
+
+            if intentos >=  max_intentos:
+                print(f"No lograste Adivinar antes de {max_intentos} intentos. PERDISTE😈😈😈")
                 break
             
+
+
         continuar = input("Desea continuar jugando (s/n): ")
         if continuar.lower() != "s":
             print("\n" + "-" * 40)
-            print("🎉 Gracias por jugar Adivina el Número 🎉")
+            print(f"🎉 Gracias por jugar Adivina el Número, Tu Puntaje fue de: {puntaje_total} 🎉")
             print("¡Hasta la próxima!")
             print("-" * 40)
 
